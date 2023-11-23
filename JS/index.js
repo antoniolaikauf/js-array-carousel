3c0558b1d23c73ab4344d95b6b0c0fe6928e00d8
antoniolaikauf <antonio.laikauf00@gmail.com>
2023-11-23 16:24:45 +0100

    fised : sistemtato pulsante grafica add: aggiunto caratteristica per comprarire pulsanti

--- JS/index.js
+++ JS/index.js
@@ -1,35 +1,42 @@
 
 // creazione dell'array con dentro le immagini
 let gruppoImmagini = document.getElementsByClassName("js-img");
-// console.log(gruppoImmagini[0]);
+
 // creazione della variabile che aggiorna l'immagine
 
 let upgrade = 0;
-// creazione dell'evento click
+// creazione dell'evento click per andare avanti
 let buttonAvanti = document.getElementById("scorrere-avanti");
 buttonAvanti.addEventListener("click", function () {
     if (upgrade <= gruppoImmagini.length -1) {
-        gruppoImmagini[upgrade].classList.remove("d-compari")      
+        // togliere classe 
+        gruppoImmagini[upgrade].classList.remove("d-compari") 
+         //incremento   
         upgrade++;
+        // aggiungere la classe 
         gruppoImmagini[upgrade].classList.add("d-compari")
-       
+        // bottone opposto rimossa classe quando ritorni indietro
+        buttonindietro.classList.remove("d-nascosta")
         if (upgrade === gruppoImmagini.length -1 ){
             buttonAvanti.classList.add("d-nascosta")
         }
     }
 })
-
+// creazione dell'evento click per tornare indietro
 let buttonindietro = document.getElementById("scorrere-indietro");
-
 buttonindietro.addEventListener("click", function () {
-    if (upgrade === gruppoImmagini.length -4 ){
-       buttonindietro.classList.add("d-nascosta")
-    }
     if (upgrade <= gruppoImmagini.length -1) {
-        gruppoImmagini[upgrade].classList.remove("d-compari")      
+        // togliere classe 
+        gruppoImmagini[upgrade].classList.remove("d-compari")  
+        // decremento    // 
         upgrade--;
+        // aggiungere la classe 
         gruppoImmagini[upgrade].classList.add("d-compari")
+         // bottone opposto rimossa classe quando vai avanti indietro
         buttonAvanti.classList.remove("d-nascosta")
+        if (upgrade === 0 ){
+            buttonindietro.classList.add("d-nascosta")
+         }
     }
 })
 
