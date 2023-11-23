c17f9c93a237e4ad3878e4d604e22ba814d6a339
antoniolaikauf <antonio.laikauf00@gmail.com>
2023-11-23 15:49:03 +0100

    feat : messo caratteristica del pulsante scorrere -avanti che si veda quando si torna indietro

--- JS/index.js
+++ JS/index.js
@@ -3,16 +3,33 @@
 let gruppoImmagini = document.getElementsByClassName("js-img");
 // console.log(gruppoImmagini[0]);
 // creazione della variabile che aggiorna l'immagine
-let upgrade = 0;
 
+let upgrade = 0;
 // creazione dell'evento click
 let buttonAvanti = document.getElementById("scorrere-avanti");
 buttonAvanti.addEventListener("click", function () {
     if (upgrade <= gruppoImmagini.length -1) {
         gruppoImmagini[upgrade].classList.remove("d-compari")      
         upgrade++;
-
         gruppoImmagini[upgrade].classList.add("d-compari")
-
+       
+        if (upgrade === gruppoImmagini.length -1 ){
+            buttonAvanti.classList.add("d-nascosta")
+        }
     }
 })
+
+let buttonindietro = document.getElementById("scorrere-indietro");
+
+buttonindietro.addEventListener("click", function () {
+    if (upgrade === gruppoImmagini.length -4 ){
+       buttonindietro.classList.add("d-nascosta")
+    }
+    if (upgrade <= gruppoImmagini.length -1) {
+        gruppoImmagini[upgrade].classList.remove("d-compari")      
+        upgrade--;
+        gruppoImmagini[upgrade].classList.add("d-compari")
+        buttonAvanti.classList.remove("d-nascosta")
+    }
+})
+
