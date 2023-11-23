4b6ed3700b05e2185be2b2ce2f183ccc7b9b0841
antoniolaikauf <antonio.laikauf00@gmail.com>
2023-11-23 21:36:10 +0100

    add: aggiunto un pezzo di bonus

--- bonus/JS/index.js
+++ bonus/JS/index.js
@@ -1,20 +1,26 @@
 
 // creazione dell'array con dentro le immagini
 let gruppoImmagini = document.getElementsByClassName("js-img");
+console.log(gruppoImmagini);
+
+let gruppoImmagini1 = document.getElementsByClassName("js-img1")
 // creazione della variabile che aggiorna l'immagine
 
-let upgrade=0;
+let upgrade=2;
 // creazione dell'evento click per andare avanti
 let buttonAvanti = document.getElementById("scorrere-avanti");
 buttonAvanti.addEventListener("click", function () {
     if (upgrade <= gruppoImmagini.length -1) {
        
         // togliere classe 
-        gruppoImmagini[upgrade].classList.remove("d-compari") 
+        gruppoImmagini1[upgrade].classList.remove("d-compari")
+        gruppoImmagini[upgrade].classList.remove("non-sfocatura") 
          //incremento   
         upgrade++;
         // aggiungere la classe 
-        gruppoImmagini[upgrade].classList.add("d-compari")
+        gruppoImmagini[upgrade].classList.add("non-sfocatura")
+        gruppoImmagini1[upgrade].classList.add("d-compari")
+        
         // bottone opposto rimossa classe quando ritorni indietro
         buttonindietro.classList.remove("d-nascosta")
         if (upgrade === gruppoImmagini.length -1 ){
@@ -27,11 +33,13 @@ let buttonindietro = document.getElementById("scorrere-indietro");
 buttonindietro.addEventListener("click", function () {
     if (upgrade <= gruppoImmagini.length -1) {
         // togliere classe 
-        gruppoImmagini[upgrade].classList.remove("d-compari")  
+        gruppoImmagini1[upgrade].classList.remove("d-compari")
+        gruppoImmagini[upgrade].classList.remove("non-sfocatura")  
         // decremento    // 
         upgrade--;
-        // aggiungere la classe 
-        gruppoImmagini[upgrade].classList.add("d-compari")
+        // aggiungere la classe
+        gruppoImmagini[upgrade].classList.add("non-sfocatura") 
+        gruppoImmagini1[upgrade].classList.add("d-compari")
          // bottone opposto rimossa classe quando vai avanti indietro
         buttonAvanti.classList.remove("d-nascosta")
         if (upgrade === 0 ){
